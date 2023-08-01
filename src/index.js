2bd5b3e9e532d4a9308b32e1a291c6f0d2a7323c
Thomas Szentirmay <tlszent@outlook.com>
2023-08-01 12:53:16 -0400

    turned into a PWA

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import './styles/main.scss';
 import NoteApp from './lib/NoteApp';
 
 NoteApp.init();
+
+if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+    navigator.serviceWorker.register('/service-worker.js');
+}
